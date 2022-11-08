cfb6aef13aaa65ec4dcb6d504d61a960a96ddb86
Matthew Juntunen <matthewjunt@tamu.edu>
2022-11-07 19:57:19 -0600

    removed test table from server

--- pos-sys/src/Server.js
+++ pos-sys/src/Server.js
@@ -11,27 +11,6 @@ const Server = () => {
             <div>
                 <button onClick={() => navigate(-1)}>Go Back Home</button>
             </div>
-            <div className="container">
-                <h1>Simple Inventory Table</h1>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Product Name</th>
-                        <th>Product Category</th>
-                        <th>Unit Price</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td/>
-                            <td/>
-                            <td/>
-                            <td/>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
         </div>
     )
 };
