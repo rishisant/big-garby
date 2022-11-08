16eab61a7589ad0968402e0505be8ee6a4a55b2d
Rishi Santhanam <rishisant@tamu.edu>
2022-11-07 19:32:48 -0600

    stash

--- pos-sys/src/Manager.js
+++ pos-sys/src/Manager.js
@@ -5,10 +5,18 @@ const Manager = () => {
     const navigate = useNavigate();
     return(
         <div>
-            <div id="logo"><img src="./src/components/logo_mini1.png"></img></div>
-            <div className = "HomeScreen-top">
-                <header id="ManagerTopText">HSS Manager View</header>
+            {/* Logo in Top Right */}
+            <div id="prelogo">
+                <img id="logo" alt="Click Here to Return Home" onClick={() => navigate(-1)} src={"https://i.imgur.com/Y2jdLdx.png"}></img>
             </div>
+            <br></br>
+
+            {/* Manager Div */}
+            <div className = "HomeScreen-top">
+                <header id="ManagerTopText">Manager</header>
+            </div>
+
+            {/* Return Home Screen */}
             <div>
                 <button onClick={() => navigate(-1)}>Go Back Home</button>
             </div>
