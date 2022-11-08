f813aea58e00d489722d6a7e6d2332fecfff822f
Rishi Santhanam <rishisant@tamu.edu>
2022-11-08 16:33:21 -0600

    changed customer front/back

--- pos-sys/src/Customer.js
+++ pos-sys/src/Customer.js
@@ -5,11 +5,28 @@ const Customer = () => {
     const navigate = useNavigate();
     return(
         <div>
-            <div>
-                Howdy this is the Customer Screen
+            {/* Logo in Top Right */}
+            <div id="prelogo">
+                <img id="logo" alt="Click Here to Return Home" onClick={() => navigate(-1)} src={"https://i.imgur.com/Y2jdLdx.png"}></img>
             </div>
-            <div>
-                <button onClick={() => navigate(-1)}>Go Back Home</button>
+            <br></br>
+
+            {/* Manager Div */}
+            <div className = "HomeScreen-top">
+                <header id="ManagerTopText">Customer</header>
+            </div>
+            <div id="disc1">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Welcome to the customer screen! Here you can place orders, view your order history, and view your current order. 
+                Click on the logo in the top-right to return to the home screen.
+            </div>
+            <br></br>
+
+            {/* Divs */}
+            <div id="allDivs">
+                <div class="box">BOX 1 (Insert a Table within this space)</div>
+                <div class="box">BOX 2 (Insert a Table within this space)</div>
+                <div class="box">BOX 3 (Insert a Table within this space)</div>
             </div>
         </div>
     )
