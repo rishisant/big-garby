89c1b295369aca255df131e9d29c18f522d37301
Rishi Santhanam <rishisant@tamu.edu>
2022-12-03 19:25:26 -0600

    stash

---
+++ pos-sys/build/precache-manifest.c1043d35ebe8b1b77bdb1672a27ebfa1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "84a3559bb9cf55b90618",
+    "url": "/static/css/main.e6a4ac15.chunk.css"
+  },
+  {
+    "revision": "84a3559bb9cf55b90618",
+    "url": "/static/js/main.15a54410.chunk.js"
+  },
+  {
+    "revision": "69cb149df85d9d92db04",
+    "url": "/static/js/runtime~main.95144799.js"
+  },
+  {
+    "revision": "680a3853bc69192e2eeb",
+    "url": "/static/js/2.2489d427.chunk.js"
+  },
+  {
+    "revision": "7621bc6736f774071bfc",
+    "url": "/static/js/3.1d9e4d91.chunk.js"
+  },
+  {
+    "revision": "6f0a0294bb05b8a8287d3d797e34668c",
+    "url": "/static/media/hss_transparent.6f0a0294.png"
+  },
+  {
+    "revision": "556297f58a1f80a16aed5fe26f89e2f8",
+    "url": "/static/media/home_transparent.556297f5.png"
+  },
+  {
+    "revision": "c003aea145276b7b9d9616eb072cdd2e",
+    "url": "/static/media/subway_photo.c003aea1.png"
+  },
+  {
+    "revision": "94fe29961ef52fd419a101469962ea50",
+    "url": "/static/media/query_transparent.94fe2996.png"
+  },
+  {
+    "revision": "2bde80f38c7d852e45a2821554fb2e2d",
+    "url": "/static/media/manager_transparent.2bde80f3.png"
+  },
+  {
+    "revision": "a023d1860ecb4ef2c23313a612922cb2",
+    "url": "/static/media/googlemaps.a023d186.png"
+  },
+  {
+    "revision": "4993ae244ae50b9363d98ff646aa2b1c",
+    "url": "/index.html"
+  }
+];
